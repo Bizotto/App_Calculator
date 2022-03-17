d7f43fb5dbf7e5f79fb1c21e03106f5f8adb3357
Bizotto <bizotto.nicolas@gmail.com>
2022-03-17 11:09:38 -0300

    display

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,22 @@
-import react from 'react';
+import React from 'react';
 import {StyleSheet, Text, Dimensions, TouchableHighLight} from 'react-native';
 
 const styles = StyleSheet.create({
   button: {
     fontSize: 40,
     height: Dimensions.get('window').width / 4,
+    width: Dimensions.get('window').width / 4,
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
   },
 });
+
+export default props => {
+  return (
+    <TouchableHighLight onPress={props.onClick}>
+      <Text Style={styles.button}>{props.label}</Text>
+    </TouchableHighLight>
+  );
+};
