688ce3a164ed8d4297a7e53bc6b85d994629f6b6
Bizotto <bizotto.nicolas@gmail.com>
2022-03-17 10:09:06 -0300

    inicio do app

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import {StyleSheet, Text, Dimensions, TouchableHighLight} from 'react-native';
+
+
+
+
+const styles =StyleSheet.create({
+  button:{
+    fontSize: 40, 
+    height:Dimensions.get ('window'). width/4
+  }
+})
